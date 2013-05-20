e598d48ba3d4008abef1e8b059c245d5e15510a0
patscan <patscan@gmail.com>
2013-05-19 17:52:03 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Photog
+- A photo-sharing app built by Pat Scanlan.
+
+My goal is to create a user-friendly interface for uploading and viewing photos, creating albums. I plan to use Rails, sass, haml, jquery-ui, and image uploading gems as needed.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photog::Application.config.secret_token = '6d493591255225f287b65973eabfbaa5b1c5c83c82162b9f6149bf474f21c87cf5ed7fa4b25734ca8d6dcea432d6fbe1b42b55200fab7e8832f9b80ae9b2dd76'
