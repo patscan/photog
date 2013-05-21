5ce3ac2e9819832549ad077febe38ca67d51b674
patscan <patscan@gmail.com>
2013-05-20 23:25:20 -0700

    create new album controller actions, photo view

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,20 @@
+class AlbumsController < ApplicationController
+  def new
+    @album = Album.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
